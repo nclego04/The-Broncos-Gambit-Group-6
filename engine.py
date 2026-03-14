855be2614237fd4ac1ac363fc13cfde43ae6ba0e
Nathan Cinco <56283492+nclego04@users.noreply.github.com>
2026-03-13 19:44:17 -0700

    Implement legal and pseudo-legal move generation

---
+++ engine.py
@@ -0,0 +1,31 @@
+def legal_moves(p):
+    out = []
+    for m in pseudo_legal_moves(p):
+        np = make_move(p, m)
+        if not in_check(np, not np.white_to_move):
+            out.append(m)
+    return out
+
+def pseudo_legal_moves(p):
+    moves = []
+    us_white = p.white_to_move
+    for i in range(64):
+        pc = p.b[i]
+        if pc == '.': continue
+        white = is_white_piece(pc)
+        if white != us_white: continue
+        
+        up = pc.upper()
+        if up == 'P':
+            moves.extend(gen_pawn(p, i, white))
+        elif up == 'N':
+            moves.extend(gen_knight(p, i, white))
+        elif up == 'B':
+            moves.extend(gen_bishop(p, i, white))
+        elif up == 'R':
+            moves.extend(gen_rook(p, i, white))
+        elif up == 'Q':
+            moves.extend(gen_queen(p, i, white))
+        elif up == 'K':
+            moves.extend(gen_king(p, i, white))
+    return moves
