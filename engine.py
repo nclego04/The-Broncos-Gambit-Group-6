2c815602a536ad6f748c23b92c7a8218e9fc44bf
Nathan Cinco <56283492+nclego04@users.noreply.github.com>
2026-03-13 19:45:28 -0700

    Remove legal_moves function
    Removed the legal_moves function that filtered valid moves.

--- engine.py
+++ engine.py
@@ -1,11 +1,3 @@
-def legal_moves(p):
-    out = []
-    for m in pseudo_legal_moves(p):
-        np = make_move(p, m)
-        if not in_check(np, not np.white_to_move):
-            out.append(m)
-    return out
-
 def pseudo_legal_moves(p):
     moves = []
     us_white = p.white_to_move
